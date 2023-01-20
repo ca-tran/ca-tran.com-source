1b91922d65c7c953f5b6fa2eaef267da352844d4
Ca Tran <catrandesign@gmail.com>
2023-01-21 03:42:22 +0700

    v1.08

--- script.js
+++ script.js
@@ -10,17 +10,33 @@ function refreshTime() {
 //DARK MODE
 function darkMode() {
   var darkElement = document.body;
+  var lightButton = document.querySelector(".light-mode-button");
   darkElement.classList.remove("light-mode");
   darkElement.classList.toggle("dark-mode");
-  var lightButton = document.querySelector(".light-mode-button");
 }
 
 //LIGHT MODE
 function lightMode() {
   var lightElement = document.body;
+  var darkButton = document.querySelector(".dark-mode-button");
   lightElement.classList.remove("dark-mode");
   lightElement.classList.toggle("light-mode");
-  var darkButton = document.querySelector(".dark-mode-button");
+}
+
+//Display Sectors
+function displaySectors() {
+  const bioSectors = document.querySelector("#bio-sectors");
+  bioSectors.innerText = "from Education, NGOs, Advertising, Healthcare, Fintech to F&B and Nightlife,";
+  const bioSectorsLink = document.querySelector("#multiple-sectors")
+  bioSectorsLink.style.textDecoration = "none";
+
+}
+
+//Display expertise
+function displayExpertise() {
+  const bioExpertise = document.querySelector("#bio-expertise");
+  bioExpertise.innerText = "graphic design & motion graphics.";
+  bioExpertise.style.textDecoration = "none";
 }
 
 //SVG TRACE
@@ -28,7 +44,6 @@ function lightMode() {
 
 // var path = document.querySelector('.path');
 // var length = path.getTotalLength()
-
 
 //CYCLE TEXT
 // var greets = $('div[id^="hello-"]').hide(),
@@ -54,3 +69,84 @@ function lightMode() {
 //   navBurger.classList.remove("active");
 //   navMenu.classList.remove("active");
 // }))
+
+
+//WUNDERGROUNDWEATHER
+$(document).ready(function () {
+	var currentDate,
+			currentLocation = 70000, // Default to Chicago
+			currentTemp = [],
+			currentUnits = 'c'; // Default to Fahrenheit
+
+	// ---------------
+	// Weather API
+	// ---------------
+	
+	// Send request to API to get weather data
+	function getWeather(location) {
+		var weatherRequest = $.ajax({
+			method: 'GET',
+			url: '' + location + '.json'
+		});
+	}
+	
+	// Grab only the needed info from weather request and return
+	function processData(data) {
+		var current = data.current_observation;
+		var daily = data.forecast.simpleforecast.forecastday;
+		// Store values for current date, location, and temp
+		currentLocation = current.display_location.city + ', ' + current.display_location.state;
+		currentTemp = {
+			c: current.temp_c,
+		};
+		forecast.length = 0; // Empty array first
+		// Display weather ONLY after processing
+		displayWeather();
+	}
+	
+	// Display data on page
+	function displayWeather() {
+		// Separate today's forecast from the rest
+		var today = forecast.shift();
+		// Today - Print weather data
+		$('#current .conditions').html(today.conditions);
+		$('#lastUpdated').html('Last updated at ' + getCurrentTime());
+		// Get/update temps with current units
+		updateTemps(currentUnits);
+	}
+	
+	// Update temps and add to page
+	function updateTemps(units) {
+		$('#current .temp').html(Math.round(currentTemp[units]));
+		$forecastDivs.each(function(index) {
+			$(this).find('.high').html(forecast[index][units].high);
+			$(this).find('.low').html(forecast[index][units].low);
+		});
+	}
+	
+	// ------------------------
+	// Functions to run onload
+	// ------------------------ 
+	window.onload = function() {
+		getWeather(currentLocation); // Default to get Chicago weather
+		// Suggest to share location with message and button animation
+	};
+});
+
+
+//openweathermap WEATHER
+// var descrip = document.querySelector('#description');
+// var temp = document.querySelector('#temp');
+
+// window.onload = function() {
+//   fetch('https://api.openweathermap.org/data/2.5/weather?id=1566083&appid=9f6562695ed85f8105bf1d55df422373&units=metric')
+//   .then(res => res.json())
+
+//   .then(data => 
+//   {
+//     var descrip = data['weather']['0']['description']
+//     var tempature = data['main']['temp']
+//     temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+//     description.innerHTML = `Conditions: <span>${descrip}<span>`
+//   })
+// };
