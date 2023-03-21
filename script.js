302046d67b730512eac76749487a5a98958bcd1a
Ca Tran <catrandesign@gmail.com>
2023-03-21 14:55:32 +0700

    v1.17

--- script.js
+++ script.js
@@ -10,34 +10,34 @@ function refreshTime() {
 
 //DARK MODE
 function darkMode() {
-  var darkElement = document.body;
-  var lightButton = document.querySelector(".light-mode-button");
+  const darkElement = document.body;
+  const lightButton = document.querySelector(".light-mode-button");
   darkElement.classList.remove("light-mode");
   darkElement.classList.toggle("dark-mode");
 }
 
 //LIGHT MODE
 function lightMode() {
-  var lightElement = document.body;
-  var darkButton = document.querySelector(".dark-mode-button");
+  const lightElement = document.body;
+  const darkButton = document.querySelector(".dark-mode-button");
   lightElement.classList.remove("dark-mode");
   lightElement.classList.toggle("light-mode");
 }
 
 //EXPERTISE
 function displayExpertise() {
-	const bioExpertise = document.querySelector("#bio-expertise");
-	bioExpertise.innerText = "art direction, print, motion & photography.";
-	bioExpertise.style.textDecoration = "none";
-	bioExpertise.style.backgroundColor = "var(--color-bg-white)";
-	bioExpertise.style.color = "var(--color-black)";  
-  }
+  const bioExpertise = document.querySelector("#bio-expertise");
+  bioExpertise.textContent = "art direction, print, motion & photography.";
+  bioExpertise.style.textDecoration = "none";
+  bioExpertise.style.backgroundColor = "var(--color-bg-white)";
+  bioExpertise.style.color = "var(--color-black)";
+}
   
 //SECTORS
 function displaySectors() {
   const bioSectors = document.querySelector(".bio-sectors");
-  bioSectors.innerText = "from Education, NGOs, Advertising, Healthcare, Fintech to F&B and Nightlife,";
-  const bioSectorsLink = document.querySelector("#multiple-sectors")
+  bioSectors.textContent = "from Education, NGOs, Advertising, Healthcare, Fintech to F&B and Nightlife,";
+  const bioSectorsLink = document.querySelector("#multiple-sectors");
   bioSectorsLink.style.textDecoration = "none";
   bioSectorsLink.style.backgroundColor = "var(--color-bg-white)";
   bioSectorsLink.style.color = "var(--color-black)";
@@ -48,20 +48,21 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < slides.length; i++) {
-    slides[i].className = slides[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  slides[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    const slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].classList.remove("active");
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].classList.add("active");
+    
+    setTimeout(showSlides, 2000);
 }
 
 //SVG TRACE
