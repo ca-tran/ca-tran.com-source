8fd6547e6401612b0f31097c49f549a6417a12e4
Ca Tran <catrandesign@gmail.com>
2023-04-20 02:33:13 +0700

    v1.6

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 //Bangkok IANA Timezone LIVE
 function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    const dateString = new Date().toLocaleTimeString("en-US", {timeZone: "Asia/Bangkok"});
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-  }
-    setInterval(refreshTime, 0);
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleTimeString("en-US", {
+    timeZone: "Asia/Bangkok",
+  });
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 0);
 
 //EXPERTISE
 function displayExpertise() {
   const bioExpertise = document.querySelector("#bio-expertise");
-  bioExpertise.textContent = "art direction, print, motion, photography, front-end and glitch art.";
+  bioExpertise.textContent =
+    "art direction, print, motion, photography & glitch art.";
   bioExpertise.style.textDecoration = "none";
   bioExpertise.style.backgroundColor = "inherit";
   bioExpertise.style.color = "inherit";
 }
-  
+
 //SECTORS
 function displaySectors() {
   const bioSectors = document.querySelector(".bio-sectors");
-  bioSectors.textContent = "from Education, NGOs, non-profit, Advertising, Healthcare, Fintech to F&B and Nightlife,";
+  bioSectors.textContent =
+    "from Education, NGOs, non-profit, Advertising, Healthcare, Fintech to F&B and Nightlife,";
   const bioSectorsLink = document.querySelector("#multiple-sectors");
   bioSectorsLink.style.textDecoration = "none";
   bioSectorsLink.style.backgroundColor = "inherit";
@@ -28,16 +32,28 @@ function displaySectors() {
 
 //FACES
 const faces = [
-  "images/faces/face-1.svg",
-  "images/faces/face-2.svg",
-  "images/faces/face-3.svg",
-  "images/faces/face-4.svg",
-  "images/faces/face-5.svg",
-  "images/faces/face-6.svg",
-  "images/faces/face-7.svg",
-  "images/faces/face-8.svg",
-  "images/faces/face-9.svg",
-  "images/faces/face-10.svg"
+  "images/faces/bro-1.svg",
+  "images/faces/bro-2.svg",
+  "images/faces/big-nose.svg",
+  "images/faces/grin.svg",
+  "images/faces/mountain-temple.svg",
+  "images/faces/one-eyed.svg",
+  "images/faces/samurai-1.svg",
+  "images/faces/samurai-3.svg",
+  "images/faces/conspire.svg",
+  "images/faces/cheeky.svg",
+  "images/faces/blah.svg",
+  "images/faces/sassy.svg",
+  "images/faces/general.svg",
+  "images/faces/dragon-dance.svg",
+  "images/faces/content.svg",
+  "images/faces/satisfied.svg",
+  "images/faces/scream.svg",
+  "images/faces/moutain-steam.svg",
+  "images/faces/meh.svg",
+  "images/faces/emo.svg",
+  "images/faces/endless.svg",
+  "images/faces/temple.svg",
 ];
 
 let currentFaceIndex = -1;
@@ -52,36 +68,39 @@ function getRandomFace() {
 }
 
 function setFace() {
-    const faceUrl = getRandomFace();
-    const faceImg = document.querySelector(".faces-img");
-    faceImg.src = faceUrl;
+  const faceUrl = getRandomFace();
+  const faceImg = document.querySelector(".faces-img");
+  faceImg.src = faceUrl;
 }
 
 setFace();
 
 window.setInterval(() => {
-    setFace();
+  setFace();
 }, 2000);
-
 
 //DISPLAY CURRENT WEATHER - OpenWeather
 const apiKey = "9f6562695ed85f8105bf1d55df422373"; // OpenWeatherMap API key
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent("Ho Chi Minh")}&units=metric&appid=${apiKey}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+  "Ho Chi Minh"
+)}&units=metric&appid=${apiKey}`;
 
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const weatherDescription = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+  .then((response) => response.json())
+  .then((data) => {
+    const weatherDescription =
+      data.weather[0].description.charAt(0).toUpperCase() +
+      data.weather[0].description.slice(1);
     const temperature = Math.round(data.main.temp);
     const city = data.name;
     const country = data.sys.country;
-    
+
     const weatherText = `${temperature}°C — ${weatherDescription}`;
-    
+
     const weatherElement = document.getElementById("weather-text");
     weatherElement.textContent = weatherText; // Set the text content of the HTML element with ID "weather-text" to the weather text
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(`Error fetching weather data: ${error}`);
   });
 
@@ -92,21 +111,52 @@ const isNighttime = currentTime.getHours() >= 19 || currentTime.getHours() < 7;
 
 if (isNighttime) {
   // Create a link element for the dark mode stylesheet
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'dark-mode.css';
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "dark-mode.css";
 
   // Add the link element to the page's head element
   document.head.appendChild(link);
 } else {
   // Remove any existing link elements for the dark mode stylesheet
-  const links = document.head.getElementsByTagName('link');
+  const links = document.head.getElementsByTagName("link");
   for (let i = 0; i < links.length; i++) {
     const link = links[i];
-    if (link.href.includes('dark-mode.css')) {
+    if (link.href.includes("dark-mode.css")) {
       document.head.removeChild(link);
     }
   }
+}
+
+// DARK LIGHT MANUAL MODE V2
+const darkModeBtn = document.querySelector(".dark-mode-button");
+const lightModeBtn = document.querySelector(".light-mode-button");
+
+function lightMode2() {
+  const links = document.head.getElementsByTagName("link");
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    if (link.href.includes("dark-mode.css")) {
+      document.head.removeChild(link);
+    }
+  }
+
+  // Set buttons
+  lightModeBtn.style.display = "none";
+  darkModeBtn.style.dislay = "inherit";
+}
+
+function darkMode2() {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "dark-mode.css";
+
+  // Add the link element to the page's head element
+  document.head.appendChild(link);
+
+  // Set buttons
+  darkModeBtn.style.dislay = "none";
+  lightModeBtn.style.display = "inherit";
 }
 
 // GREETS BASED ON TIME
@@ -117,13 +167,13 @@ if (isNighttime) {
 //   timeGreets.textContent = "G'day,";
 // };
 // if (isAfternoon) {
-//   timeGreets.textContent = "Good afternoon,";
+//   timeGreets.textContent = "G'afternoon,";
 // };
 // if (isEvening) {
-//   timeGreets.textContent = "Good evening,";
+//   timeGreets.textContent = "G'evening,";
 // };
 // if (isMidnight) {
-//   timeGreets.textContent = "Good night,";
+//   timeGreets.textContent = "G'night,";
 // };
 
 //RANDOM IMG OVERLAY
@@ -146,7 +196,7 @@ const images = [
   "images/overlay-images/artwork/screenprint4.jpg",
   "images/overlay-images/artwork/uss-arkansas-gif.gif",
   "images/overlay-images/artwork/War-&-Civilization.gif",
-  "images/overlay-images/artwork/War-Horse.gif"
+  "images/overlay-images/artwork/War-Horse.gif",
 ];
 
 let currentIndex = -1;
@@ -161,55 +211,145 @@ function getRandomImage() {
 }
 
 function setOverlayImage() {
-    const imageUrl = getRandomImage();
-    const overlayImg = document.querySelector(".overlay-img");
-    overlayImg.src = imageUrl;
+  const imageUrl = getRandomImage();
+  const overlayImg = document.querySelector(".overlay-img");
+  overlayImg.src = imageUrl;
 }
 
 setOverlayImage();
 
 window.setInterval(() => {
-    setOverlayImage();
+  setOverlayImage();
 }, 4000);
-
 
 //AUTO COPYRIGHT CURRENT YEAR, I'M LAZY TO UPDATE MY WEB
 const currentYear = document.getElementById("current-year");
 
-window.onload = function() {
-    const getCurrentYear = new Date().getFullYear();
-    currentYear.textContent = getCurrentYear;
-}
+window.onload = function () {
+  const getCurrentYear = new Date().getFullYear();
+  currentYear.textContent = getCurrentYear;
+};
 
 //SEE MORE BRANDS
 const overseasBrands = document.querySelector("#overseas-brands");
 const seeMoreBrandsBtn = document.querySelector(".see-more-brands-btn");
-const seeMoreBrandsBar = document.querySelector(".gradident-bar-brands")
+const seeMoreBrandsBar = document.querySelector(".gradident-bar-brands");
 
 function seeMoreBrands() {
   overseasBrands.style.visibility = "visible";
   overseasBrands.style.opacity = "1";
   overseasBrands.style.display = "inherit";
-  seeMoreBrandsBtn.style.display= "none";
+  seeMoreBrandsBtn.style.display = "none";
   seeMoreBrandsBtn.style.opacity = "0";
-  seeMoreBrandsBar.style.dislay="none";
+  seeMoreBrandsBar.style.dislay = "none";
   seeMoreBrandsBar.style.opacity = "0";
 }
 
 //SEE MORE EXP
 const moreExp = document.querySelector("#more-exp");
 const seeMoreExpBtn = document.querySelector(".see-more-exp-btn");
-const seeMoreExpBar = document.querySelector(".gradident-bar-exp")
+const seeMoreExpBar = document.querySelector(".gradident-bar-exp");
 
 function seeMoreExp() {
   moreExp.style.visibility = "visible";
   moreExp.style.opacity = "1";
   moreExp.style.display = "inherit";
-  seeMoreExpBtn.style.display= "none";
+  seeMoreExpBtn.style.display = "none";
   seeMoreExpBtn.style.opacity = "0";
-  seeMoreExpBar.style.dislay="none";
+  seeMoreExpBar.style.dislay = "none";
   seeMoreExpBar.style.opacity = "0";
 }
+
+// OVERLAY IMGS
+// Array of image URLs
+const photographyImages = [
+  "images/photography/0037_17.jpg",
+  "images/photography/0038_17A.jpg",
+  "images/photography/0039_18.jpg",
+  "images/photography/0040_18A.jpg",
+  "images/photography/0041_19.jpg",
+];
+
+let currentPhotographyIndex = -1;
+
+function getRandomPhotographyImage() {
+  let photographyIndex = Math.floor(Math.random() * photographyImages.length);
+  if (photographyIndex === currentPhotographyIndex) {
+    photographyIndex = (photographyIndex + 1) % photographyImages.length;
+  }
+  currentPhotographyIndex = photographyIndex;
+  return photographyImages[currentPhotographyIndex];
+}
+
+function setOverlayPhotographyImage() {
+  const photographyImageUrl = getRandomPhotographyImage();
+  const overlayPhotographyImg = document.querySelector(".photo-overlay-img");
+  overlayPhotographyImg.src = photographyImageUrl;
+}
+
+setOverlayPhotographyImage();
+
+window.setInterval(() => {
+  setOverlayPhotographyImage();
+}, 4000);
+
+function showImages() {
+  const photographyContainer = document.getElementsByID("hover-images");
+  const photographyContainer2 =
+    document.getElementsByClassName("photo-overlay-img");
+  // Show the container
+  photographyContainer.style.display = "flex";
+  photographyContainer2.style.display = "flex";
+}
+
+function hideImages() {
+  // Hide the container
+  photographyContainer.style.display = "none";
+}
+
+// MARQUEE V2
+// Get the container and content elements
+const container = document.querySelector(".marquee-container");
+const content = document.querySelector(".marquee-content");
+
+// Get the width of the container and content
+const containerWidth = container.offsetWidth;
+const contentWidth = content.offsetWidth;
+
+// Clone the content element and add it to the container
+const clonedContent = content.cloneNode(true);
+container.appendChild(clonedContent);
+
+// Define the animation function
+function animateMarquee() {
+  // Move the container to the left by 1 pixel
+  container.style.transform = "translateX(-1px)";
+
+  // If the content has scrolled offscreen, reset its position
+  if (container.offsetLeft < -contentWidth) {
+    container.style.transform = `translateX(${containerWidth}px)`;
+  }
+
+  // Repeat the animation every frame
+  requestAnimationFrame(animateMarquee);
+}
+
+// Start the animation
+animateMarquee();
+
+//GALLERY
+
+// GRAB CONTACT EMAIL FORM
+// const form = document.getElementById("contact-form");
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
+//   const message = document.getElementById("message").value;
+//   const subject = `New message from ${name}`;
+//   const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage:%0D%0A${message}`;
+//   window.location.href = `mailto:hello@ca-tran.com?subject=${subject}&body=${body}`;
+// });
 
 //LIGHT DARK MODE MANUAL
 // //DARK MODE
